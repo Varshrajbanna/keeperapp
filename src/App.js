03a7af0591b17c65f6d6b12de8f403d740020c42
Varshrajbanna <justvarshrajsinghsherpur@gmail.com>
2024-05-19 00:57:39 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+import Header from ".Header";
+import Footer from ".Footer";
+import Notes from ".Notes";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Notes />
+    </div>
+  );
+}
---
+++ src/Notes.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+function Notes() {
+  return (
+    <div className="notes">
+      <h1>This is title</h1>
+      <p>these is content</p>
+    </div>
+  );
+}
+export default Notes;
